15f11ea94c077c6ed6dc7bc97bdb841f3f69103e
Mikedave001 <itsmikel89@gmail.com>
2026-09-14 02:08:30 +0100

    working on files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const mongoose = require('mongoose');
+const userRoute = require("./routes/userRoutes.js")
+const productRoute = require("./routes/productRoutes.js")
+
+const compass_string = "mongodb://localhost:27017/cohort8_db"
+const atlas_string = "mongodb+srv://itsmikel89_db_user:itsmikel89_db_user@cluster0.gv4npmt.mongodb.net/cohort8_db?appName=Cluster0"
+
+mongoose.connect(compass_string)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("Connection Error: ", err));
+
+
+const app = express()
+const port = 6666
+
+
+app.use(express.json())
+
+app.get("/" , (req , res)=>{
+    res.send("server is active")
+})
+app.use("/users", userRoute)
+app.use("/products", productRoute)
+app.listen(port , ()=>{
+    console.log(`server is up and running on port : ${port}`)
+})
