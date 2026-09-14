9043dd8109563d9b99e7629751a8eac8e2412846
Mikedave001 <itsmikel89@gmail.com>
2026-09-14 13:28:33 +0100

    added some changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const productRoute = require("./routes/productRoutes.js")
 const compass_string = process.env.COMPASS_STRING
 const atlas_string = process.env.ATLASS_STRING
 
-mongoose.connect(compass_string)
+mongoose.connect(atlas_string)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("Connection Error: ", err));
 
